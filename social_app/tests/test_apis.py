501a8bc8bbb9895a0c1a60a3a13c494d0af86cfa
akshaymorale2 <97449245+akshaymorale2@users.noreply.github.com>
2022-10-04 13:24:22 +0530

    Add files via upload

---
+++ social_app/tests/test_apis.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append(r"/Users/akshaymorale/IdeaProjects/Flask_app/")
+import json
+from ..social_app import db
+from ..social_app.social import create_app
+from unittest import TestCase
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        db.create_all(app=self.app)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all(app=self.app)
+
+    def test_signup_user(self):
+        payload_register = json.dumps({
+            'contactNumber': "9822902121",
+            'dateOfBirth': "2008-07-05T18:30:00.000Z",
+            'email': "ironman@gmail.com",
+            'first_name': "Robert",
+            'gender': "Male",
+            'is_verified': True,
+            'last_name': "Downey",
+            'password': "12345",
+            'username': "ironman"
+        })
+        response = self.client.post('/user/register', headers={"Content-Type": "application/json"}, data=payload_register)
+        # success
+        assert response.status_code == 200
+
+    def test_social_login_user(self):
+        payload = json.dumps({
+            'contactNumber': "9022902121",
+            'dateOfBirth': "1992-07-05T18:30:00.000Z",
+            'email': "minato@gmail.com",
+            'first_name': "Minato",
+            'gender': "Male",
+            'is_verified': True,
+            'last_name': "Uzumaki",
+            'password': "12345",
+            'username': "minato"
+        })
+
+        payload_login = json.dumps({
+            'username': 'minato',
+            'password': "12345"
+        })
+        self.client.post('/user/register', headers={"Content-Type": "application/json"}, data=payload)
+        response_login = self.client.post('/user/login', headers={"Content-Type": "application/json"}, data=payload_login)
+
+        # success
+        assert response_login.status_code == 200
+
