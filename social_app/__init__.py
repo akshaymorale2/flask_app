fa5754db06b5bafc52f5126889ec418078b99c3d
akshaymorale2 <97449245+akshaymorale2@users.noreply.github.com>
2022-10-06 14:26:05 +0530

    Update __init__.py

--- social_app/__init__.py
+++ social_app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
-def create_database():
+def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
